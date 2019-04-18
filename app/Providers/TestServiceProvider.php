56501c1e37d0660ca5fa127e8dadca8e14ff5523
topot <topotru@gmail.com>
2019-04-18 23:23:37 +0300

    fix

--- app/Providers/TestServiceProvider.php
+++ app/Providers/TestServiceProvider.php
@@ -2,39 +2,36 @@
 
 namespace App\Providers;
 
+use App\Services\Di\Interfaces\FirstInterface;
+use App\Services\Di\Interfaces\SecondInterface;
+use App\Services\Di\MockFirstClass;
+use App\Services\Di\MockSecondClass;
 use Illuminate\Support\ServiceProvider;
 
 class TestServiceProvider extends ServiceProvider
 {
     /**
      * Register services.
-     *
      * @return void
      */
     public function register()
     {
-        $this->app->bind(\App\Services\Di\Interfaces\FirstInterface::class, \App\Services\Di\MockFirstClass::class);
-        $this->app->bind(\App\Services\Di\Interfaces\SecondInterface::class, \App\Services\Di\MockSecondClass::class);
-        $this->app->bind(\App\Services\Di\Interfaces\SecondInterface::class, \App\Services\Di\MockSecondClass::class);
-        $this->app->bind(\App\Services\Di\Interfaces\SecondInterface::class, \App\Services\Di\MockSecondClass::class);
-        $this->app->bind(\App\Services\Di\Interfaces\SecondInterface::class, \App\Services\Di\MockSecondClass::class);
-        $this->app->bind(\App\Services\Di\Interfaces\SecondInterface::class, \App\Services\Di\MockSecondClass::class);
-        $this->app->bind(\App\Services\Di\Interfaces\SecondInterface::class, \App\Services\Di\MockSecondClass::class);
+        $this->app->bind(FirstInterface::class, MockFirstClass::class);
+        $this->app->bind(SecondInterface::class, MockSecondClass::class);
         
         $this->app->make(\App\Services\Di\HandlerClass::class);
 //        dd($this->app->runningInConsole());
     }
-
+    
     /**
      * Bootstrap services.
-     *
      * @return void
      */
     public function boot()
     {
         //
     }
-    
+
 //    public $singletons = [
 //        \App\Services\Di\Interfaces\FirstInterface::class => \App\Services\Di\FirstClass::class
 //    ];
